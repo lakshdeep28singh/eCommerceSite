79d41286856b56b8561e41cb25790347ad22b53a
Lakshdeep Singh <lakshdeep28singh@outlook.com>
2026-02-20 23:35:03 -0500

    final

--- wp-content/themes/custom-theme/index.php
+++ wp-content/themes/custom-theme/index.php
@@ -7,3 +7,35 @@
  * @package custom-theme
  * @since 1.0.0
  */
+
+
+<?php
+/* Template Name: Custom Page Template */
+get_header(); 
+?>
+
+<main id="main-content" class="site-main">
+    <div class="container">
+        <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+        ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <h1 class="entry-title"><?php the_title(); ?></h1>
+                    </header>
+
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+        <?php
+            endwhile;
+        else :
+            echo '<p>No content found.</p>';
+        endif;
+        ?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
